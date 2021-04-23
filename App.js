0f3cf5535cc792246f1a254c18606975ef625cf3
darth-roman <chouchane.louai20@gmail.com>
2021-04-24 01:45:09 +0200

    Init of the intiial structure of the project

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const App = express()
-
 const PORT = process.env.PORT || 8081
 
+App.set('view engine', 'ejs');
 
+
+App.use(express.static('public'))
+
+
+App.get('/', (req, res)=>{
+    res.render('index')
+})
 
 
 
